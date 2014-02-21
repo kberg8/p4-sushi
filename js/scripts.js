e686440ba687eaa8dfb7dfd9b33f7c91c1804ba3
kberg8 <kberg@Kelseys-iMac.local>
2014-02-21 10:29:57 -0800

    7

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ $(document).ready(function(){
 
 	$('.fancybox').fancybox();
 
+	// Map Location //
+	new Maplace({
+	show_markers: true,
+	locations: [{
+		lat: 47.659381, 
+		lon: -117.4236639,
+		zoom: 18
+		}]
+	}).Load();
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
