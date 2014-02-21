02d7af236dbf929b8c3b26fe10ebb4d168d36704
kberg8 <kberg@Kelseys-iMac.local>
2014-02-21 10:26:24 -0800

    6

---
+++ img/Sushi Maru_files/scripts.js
@@ -0,0 +1,13 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// My Awesome Scripts!
+$(document).ready(function(){
+
+	$("h4").lettering();
+
+	$('.fancybox').fancybox();
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
